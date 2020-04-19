7b55cb0d506c389bdd42e792573ec3e65113d6b4
chenhongfei <chenhongfei@ruijie.com.cn>
2020-04-19 20:38:00 +0800

    动态代理03

---
+++ design/src/main/java/com/design/proxy/dynamic3han/ITeacherDao.java
@@ -0,0 +1,8 @@
+package com.design.proxy.dynamic3han;
+
+//接口
+public interface ITeacherDao {
+    void teach();
+
+    String say(String name);
+}
---
+++ design/src/main/java/com/design/proxy/dynamic3han/TeacherDao.java
@@ -0,0 +1,15 @@
+package com.design.proxy.dynamic3han;
+
+//目标对象
+public class TeacherDao implements ITeacherDao {
+    @Override
+    public void teach() {
+        System.out.println("老师正在授课中...");
+    }
+
+    @Override
+    public String say(String name) {
+        return "hello" + name;
+    }
+
+}
